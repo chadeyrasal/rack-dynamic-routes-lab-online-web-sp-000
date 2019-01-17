979a725abfb1ab6b6b9aa0fa96f691426346cfc7
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-17 15:46:29 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,21 @@
 class Application
 
-  Item.new("Tome de chevre", 4.58)
-  Item.new("Foie gras de canard", 12.73)
-
   def call(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      if item = @@items.find{ |i| i.name == item_name }
+        resp.write item.price
+      else
+        resp.status = 400
+        resp.write "Item not found"
+      end
+    else
+      resp.status = 404
+      resp.write "Route not found"
+    end
+    resp.finish
+  end
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
