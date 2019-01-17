f341ebc51b5d8dac208e0eb9e0a890ba6ea14948
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-17 15:48:26 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -26,7 +26,7 @@ class Application
 #    req = Rack::Request.new(env)
 
 #    if req.path.match(/items/)
-#      item_name = req.path.split("items").last
+#      item_name = req.path.split("/items/").last
 #      if my_item = @@items.find{|item| item.name == item_name}
 #        resp.write "#{my_item.price}"
 #      else
