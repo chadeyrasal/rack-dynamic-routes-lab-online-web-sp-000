6a1c648893ee105c4dea0191d1c995190473c37b
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-17 15:46:59 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,12 @@
 class Application
 
+#  Item.new("Tome de chevre", 4.58)
+#  Item.new("Foie gras de canard", 12.73)
+
   def call(env)
+
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
@@ -16,23 +22,23 @@ class Application
     end
     resp.finish
   end
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
+#    resp = Rack::Response.new
+#    req = Rack::Request.new(env)
 
-    if req.path.match(/items/)
-      item_name = req.path.split("items").last
-      if my_item = @@items.find{|item| item.name == item_name}
-        resp.write "#{my_item.price}"
-      else
-        resp.status = 400
-        resp.write "Item not found"
-      end
-    else
-      resp.status = 404
-      resp.write "Route not found"
-    end
+#    if req.path.match(/items/)
+#      item_name = req.path.split("items").last
+#      if my_item = @@items.find{|item| item.name == item_name}
+#        resp.write "#{my_item.price}"
+#      else
+#        resp.status = 400
+#        resp.write "Item not found"
+#      end
+#    else
+#      resp.status = 404
+#      resp.write "Route not found"
+#    end
 
-    resp.finish
-  end
+#    resp.finish
+#  end
 
 end
